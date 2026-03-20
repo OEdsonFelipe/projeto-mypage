114a9f9813bbc813c75c9dbede416c06e857e006
OEdsonFelipe <felipeedsonnba@gmail.com>
2026-03-20 16:30:21 -0400

    Primeiro Commit do mypage

---
+++ minha-ficha-de-treino/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer>
+            <small> &copy; Edson Felipe. Todos os direitos reservados. </small>
+        </footer>
+    )
+}
+export default Footer
